a92187428d3a1f7647f97bb16439533db8c05e20
remote-torrent-6747 <abbyc@sky.com>
2021-01-24 14:57:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,29 +11,24 @@ class Waiter
 
   def self.all
     @@all
-  end
-  #created with the waiter's name & experience
+  end     #created with the waiter's name & experience
 
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
-  end
-  #create a meal, using the Class
+  end   #create a meal, using the Class
 
   def meals
     Meal.all.select do |meal|
       meal.waiter == self #checking for waiter now
     end
-  end
-  #iterating through every instance of meal and returning only those
-  #where the meal's waiter is the waiter in question
-
+  end    #iterating through every instance of meal and returning only those
+         #where the meal's waiter is the waiter in question
 
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
     end
     best_tipped_meal.customer
-  end
-  #using meals, we can order which customers are the best tipper
+  end     #using meals, we can order which customers are the best tipper
 
 end
